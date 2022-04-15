2d5943f04c3fca16ec4546209523bb092d61fb8a
mnatsyan <aramnatsyan018@gmail.com>
2022-04-16 00:52:53 +0400

    created home controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
